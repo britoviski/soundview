a23a196b03a2b6764ae3795985e79619e5172028
britoviski <98991610+britoviski@users.noreply.github.com>
2025-06-02 21:29:37 -0300

    marca

--- resources/views/categoria/categoria_create.blade.php
+++ resources/views/categoria/categoria_create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Categoria - CREATE</div>
+                <div class="card-header">Marca - CREATE</div>
 
                 <div class="card-body">
 
